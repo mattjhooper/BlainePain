60eaf723413e8f8c31ef84f2f4b697a352db5995
Matthew Hooper <mhooper@scottlogic.com>
2020-10-31 18:08:14 +0000

    Add options

--- BlainePain/Program.cs
+++ BlainePain/Program.cs
@@ -10,11 +10,31 @@ namespace BlainePain
     {
         static int Main(string[] args)
         {
-            string track = System.IO.File.ReadAllText(@"Tracks\track2.txt");           
             int collideCounter;
-            //collideCounter = Dinglemouse.TrainCrash(track, "Aaaa", 147, "Bbbbbbbbbbb", 288, 1000);
-            //collideCounter = Dinglemouse.TrainCrash(track, "Eeeeeeeeeeeeeeeeeeeeeeeeeeeeeee", 7, "Xxxx", 0, 0);
-            collideCounter = Dinglemouse.TrainCrash(track, "aA", 10, "bbbbbB", 30, 200, true);
+            string track;
+
+            string option = args.Length == 0 ? "0" : args[0];
+
+            switch(option)
+            {
+                case "0":
+                    track = System.IO.File.ReadAllText($"Tracks\\track{option}.txt"); 
+                    collideCounter = Dinglemouse.TrainCrash(track, "Aaaa", 147, "Bbbbbbbbbbb", 288, 1000, true);
+                    break;
+                case "1":
+                    track = System.IO.File.ReadAllText($"Tracks\\track{option}.txt"); 
+                    collideCounter = Dinglemouse.TrainCrash(track, "aA", 10, "bbbbbB", 30, 200, true); 
+                    break;
+
+                case "2":
+                    track = System.IO.File.ReadAllText($"Tracks\\track{option}.txt"); 
+                    collideCounter = Dinglemouse.TrainCrash(track, "Eee", 10, "aaA", 20, 100, true);
+                    break;  
+                default:
+                    throw new InvalidOperationException($"Invalid option: {option}.");   
+
+            }
+            
             return collideCounter;                        
         }
     }
