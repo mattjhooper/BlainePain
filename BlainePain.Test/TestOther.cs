9a475db554228a5bf508184ff2af6ca262ff34dc
Matthew Hooper <mhooper@scottlogic.com>
2020-10-31 10:25:19 +0000

    Removed UpdatePositionAndDirection

--- BlainePain.Test/TestOther.cs
+++ BlainePain.Test/TestOther.cs
@@ -28,67 +28,15 @@ namespace BlainePain.Test
         }
     }
     
-    public class TestFindNext
+    public class TestGetTrack
     {
         [Theory]
-        [InlineData("----------", 0, 0, Direction.East, "----------")]
-        [InlineData("|\n|\n|", 0, 0, Direction.South, "|||")]
-        [InlineData("----------", 9, 0, Direction.West, "----------")]        
-        [InlineData("|\n|\n|", 0, 2, Direction.North, "|||")]
-        [InlineData("\\\n \\\n  \\", 0, 0, Direction.Southeast, @"\\\")]
-        [InlineData("\\\n \\\n  \\", 2, 2, Direction.Northwest, @"\\\")]
-        [InlineData("  /\n /\n/", 0, 2, Direction.Northeast, @"///")]
-        [InlineData("  /\n /\n/", 2, 0, Direction.Southwest, @"///")]
-        [InlineData("|\n|\n\\-----", 0, 0, Direction.South, @"||\-----")]        
-        [InlineData("|\n|\n\\-----", 5, 2, Direction.West, @"-----\||")]
-        [InlineData("     |\n     |\n-----/", 0, 2, Direction.East, @"-----/||")]
-        [InlineData("     |\n     |\n-----/", 5, 0, Direction.South, @"||/-----")]  
-        [InlineData("/-----\n|\n|", 0, 2, Direction.North, @"||/-----")]
-        [InlineData("/-----\n|\n|", 5, 0, Direction.West, @"-----/||")] 
-        [InlineData("-----\\ \n     | \n     | ", 0, 0, Direction.East, @"-----\||")]
-        [InlineData("-----\\ \n     | \n     | ", 5, 2, Direction.North, @"||\-----")] 
-        [InlineData("-----\\\n      \\-----", 0, 0, Direction.East, @"-----\\-----")]
-        [InlineData("-----\\\n      \\-----", 11, 1, Direction.West, @"-----\\-----")]
-        [InlineData(" |\n /\n/\n|", 1, 0, Direction.South, @"|//|")]
-        [InlineData(" |\n /\n/\n|", 0, 3, Direction.North, @"|//|")]
-        [InlineData("   |\n---+---\n   |", 3, 0, Direction.South, @"|+|")]
-        [InlineData("   |\n---+---\n   |", 0, 1, Direction.East, @"---+---")]
-        [InlineData("  \\ /\n   X\n  / \\", 2, 0, Direction.Southeast, @"\X\")]
-        [InlineData("  \\ /\n   X\n  / \\", 2, 2, Direction.Northeast, @"/X/")]
-        [InlineData("   /\n---+---\n   /", 3, 2, Direction.North, @"/+/")]
-        [InlineData("   /\n---+---\n   /", 3, 0, Direction.Southwest, @"/+/")]
-        [InlineData("   | /\n  /+/\n / |", 1, 2, Direction.Northeast, @"//+//")]
-        public void TestTracks(string track, int x, int y, Direction startDirection, String desiredResult)
-        {
-            // Arrange
-            var grid = new Grid(track);
-            Coord pos = new Coord(x, y);
-            var direction = startDirection;
-            var resultTrack = new StringBuilder();
-            bool moreTrack = true;
-            int i = 0;
-            
-            // Act
-            do
-            {
-                char piece = grid.GetValue(pos);
-                resultTrack.Append(piece);
-                moreTrack = Dinglemouse.UpdatePositionAndDirection(grid, ref pos, ref direction);
-                i++; 
-
-            } while (moreTrack && i < 20);
-            
-            // Assert
-            resultTrack.ToString().Should().Be(desiredResult);
-        }
-
-        [Theory]
-        [InlineData("----------", 0, 0, Direction.East, "----------")]
-        [InlineData("     |\n     |\n-----/", 0, 2, Direction.East, @"-----/||")]
-        [InlineData("-----\\ \n     | \n     | ", 0, 0, Direction.East, @"-----\||")]
-        [InlineData("-----\\\n      \\-----", 0, 0, Direction.East, @"-----\\-----")]
-        [InlineData("   |\n---+---\n   |", 0, 1, Direction.East, @"---+---")]
-        public void TestTracks2(string trackStr, int x, int y, Direction startDirection, String desiredResult)
+        [InlineData("----------", 0, 0, "----------")]
+        [InlineData("     |\n     |\n-----/", 0, 2, @"-----/||")]
+        [InlineData("-----\\ \n     | \n     | ", 0, 0, @"-----\||")]
+        [InlineData("-----\\\n      \\-----", 0, 0, @"-----\\-----")]
+        [InlineData("   |\n---+---\n   |", 0, 1, @"---+---")]
+        public void CanGetTrack(string trackStr, int x, int y, String desiredResult)
         {
             // Arrange
             var grid = new Grid(trackStr);
@@ -129,7 +77,7 @@ namespace BlainePain.Test
         [InlineData("   /\n---+---\n   /", 3, 2, Direction.North, @"/+/")]
         [InlineData("   /\n---+---\n   /", 3, 0, Direction.Southwest, @"/+/")]
         [InlineData("   | /\n  /+/\n / |", 1, 2, Direction.Northeast, @"//+//")]
-        public void TestTracks3(string track, int x, int y, Direction startDirection, String desiredResult)
+        public void CanGetNextTrackPiece(string track, int x, int y, Direction startDirection, String desiredResult)
         {
             // Arrange
             var grid = new Grid(track);
--- BlainePain/Dinglemouse.cs
+++ BlainePain/Dinglemouse.cs
@@ -164,14 +164,17 @@ namespace BlainePain
             Coord pos = new Coord(start);
             var direction = Direction.East;
             var track = new Track();
+            var nav = new GridNavigator();
 
             do
             {
                 char piece = grid.GetValue(pos);
                 track.AddTrackPiece(piece, new Coord(pos));
 
-                moreTrack = Dinglemouse.UpdatePositionAndDirection(grid, ref pos, ref direction);
-
+                var res = GetNextTrackPiece(nav, grid, pos, direction);
+                moreTrack = res.Found;
+                pos = res.NextPos;
+                direction = res.NextDir;                
             } while (moreTrack && !pos.Equals(start));
 
             return track;
@@ -205,227 +208,6 @@ namespace BlainePain
             while (timer < limit);
 
             return -1;
-        }
-
-        public static bool UpdatePositionAndDirection(IGrid grid, ref Coord pos, ref Direction direction)
-        {
-            char gridVal;
-            bool movedDown = false;
-            bool movedUp = false;
-            bool movedLeft = false;
-            bool movedRight = false;
-            switch (direction)
-            {
-                case Direction.North:
-                    if (pos.y > 0)
-                    {
-                        pos.MoveUp();
-                        movedUp = true;
-                        gridVal = grid.GetValue(pos);
-                        if (gridVal == '/')
-                            direction = Direction.Northeast;
-                        else if (gridVal == '\\')
-                            direction = Direction.Northwest;                        
-                    }
-                    else
-                    {
-                        return false;
-                    }
-                    break;
-                case Direction.South:
-                    if (pos.y < grid.MaxY)
-                    {
-                        pos.MoveDown();
-                        movedDown = true;
-                        gridVal = grid.GetValue(pos);
-                        if (gridVal == '/')
-                            direction = Direction.Southwest;
-                        else if (gridVal == '\\')
-                            direction = Direction.Southeast;                        
-                    }
-                    else
-                    {
-                        return false;
-                    }
-                    break;
-                case Direction.East:
-                    if (pos.x < grid.MaxX)
-                    {
-                        pos.MoveRight();
-                        movedRight = true;
-                        gridVal = grid.GetValue(pos);
-                        if (gridVal == '/')
-                            direction = Direction.Northeast;
-                        else if (gridVal == '\\')
-                            direction = Direction.Southeast;                        
-                    }
-                    else
-                    {
-                        return false;
-                    }
-                    break;  
-                case Direction.West:
-                    if (pos.x > 0)
-                    {
-                        pos.MoveLeft();
-                        movedLeft = true;
-                        gridVal = grid.GetValue(pos);
-                        if (gridVal == '/')
-                            direction = Direction.Southwest;
-                        else if (gridVal == '\\')
-                            direction = Direction.Northwest;                        
-                    }
-                    else
-                    {
-                        return false;
-                    }
-                    break;    
-                case Direction.Northwest:
-                    gridVal = ' ';
-                    if (pos.y > 0)
-                    {
-                        pos.MoveUp();
-                        movedUp = true;
-                        gridVal = grid.GetValue(pos);
-                    }
-                    if (gridVal == ' ' && pos.x > 0)
-                    {
-                        pos.MoveLeft();
-                        movedLeft = true;
-                        gridVal = grid.GetValue(pos);
-                    }
-                    if (movedUp && movedLeft)
-                    {
-                        // Check final position
-                        var checkPos = new Coord(pos).MoveDown();
-                        var checkVal = grid.GetValue(checkPos);
-
-                        if (checkVal == '+' || (gridVal == ' ' && checkVal != ' '))
-                        {
-                            pos.MoveTo(checkPos);
-                            movedDown = true;
-                            gridVal = checkVal;
-                        } 
-                    }
-                    if (gridVal == '-' || (movedDown && gridVal == '+'))
-                        direction = Direction.West;
-                    else if (gridVal == '|' || gridVal == '+')
-                        direction = Direction.North;
-                    else if (gridVal == ' ')
-                        return false;
-                    
-                    break;  
-                    
-                case Direction.Northeast:
-                    gridVal = ' ';
-                    if (pos.y > 0)
-                    {
-                        pos.MoveUp();
-                        movedUp = true;
-                        gridVal = grid.GetValue(pos);
-                    }
-                    if (gridVal == ' ' && pos.x < grid.MaxX)
-                    {
-                        pos.MoveRight();
-                        movedRight = true;
-                        gridVal = grid.GetValue(pos);
-                    }
-                    if (movedUp && movedRight)
-                    {
-                        // Check final position
-                        var checkPos = new Coord(pos).MoveDown();
-                        var checkVal = grid.GetValue(checkPos);
-
-                        if (checkVal == '+' || (gridVal == ' ' && checkVal != ' '))
-                        {
-                            pos.MoveTo(checkPos);
-                            movedDown = true;
-                            gridVal = checkVal;
-                        }     
-                    }
-                    if (gridVal == '-' || (movedDown && gridVal == '+'))
-                        direction = Direction.East;
-                    else if (gridVal == '|' || gridVal == '+')
-                        direction = Direction.North;
-                    else if (gridVal == ' ')
-                        return false;
-                    
-                    break;    
-                case Direction.Southeast:
-                    gridVal = ' ';
-                    if (pos.x < grid.MaxX)
-                    {
-                        pos.MoveRight();
-                        movedRight = true;
-                        gridVal = grid.GetValue(pos);
-                    }
-                    if (gridVal == ' ' && pos.y < grid.MaxY)
-                    {
-                        pos.MoveDown();
-                        movedDown = true;
-                        gridVal = grid.GetValue(pos);
-                    }
-                    if (movedRight && movedDown)
-                    {
-                        // Check final position
-                        var checkPos = new Coord(pos).MoveLeft();
-                        var checkVal = grid.GetValue(checkPos);
-
-                        if (checkVal == '+' || (gridVal == ' ' && checkVal != ' '))
-                        {
-                            pos.MoveTo(checkPos);
-                            movedLeft = true;
-                            gridVal = checkVal;
-                        }                        
-                    }
-                    if (gridVal == '-'|| (!movedLeft && gridVal == '+'))
-                        direction = Direction.East;
-                    else if (gridVal == '|' || gridVal == '+')
-                        direction = Direction.South;
-                    else if (gridVal == ' ')
-                        return false;
-                    
-                    break; 
-                case Direction.Southwest:
-                    gridVal = ' ';
-                    if (pos.x > 0)
-                    {
-                        pos.MoveLeft();
-                        movedLeft = true;
-                        gridVal = grid.GetValue(pos);
-                    }
-                    if (gridVal == ' ' && pos.y < grid.MaxY)
-                    {
-                        pos.MoveDown();
-                        movedDown = true;
-                        gridVal = grid.GetValue(pos);
-                    }
-                    if (movedLeft && movedDown)
-                    {
-                        // Check final position
-                        var checkPos = new Coord(pos).MoveRight();
-                        var checkVal = grid.GetValue(checkPos);
-
-                        if (checkVal == '+' || (gridVal == ' ' && checkVal != ' '))
-                        {
-                            pos.MoveTo(checkPos);
-                            movedRight = true;
-                            gridVal = checkVal;
-                        }                                                     
-                    }
-                    if (gridVal == '-' || (!movedRight && gridVal == '+'))
-                        direction = Direction.West;
-                    else if (gridVal == '|' || gridVal == '+')
-                        direction = Direction.South;
-                    else if (gridVal == ' ')
-                        return false;
-                    
-                    break; 
-                                        
-                default:
-                    return false;
-            }
-            return true;
         }
     }
 }
